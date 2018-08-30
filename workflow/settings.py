071095baca2d2dc6c5f024e4d9cd0f85b2387459
Aakash Kumar Das <cse.inspire@gmail.com>
2018-08-30 14:52:34 +0530

    Update settings.py

--- workflow/settings.py
+++ workflow/settings.py
@@ -123,7 +123,7 @@ AUTH_USER_MODEL = "core.User"
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'aakashjsr@gmail.com'
-EMAIL_HOST_PASSWORD = 'aakashloyola2408'
+EMAIL_HOST_USER = 'jebotara.steel@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwe1234??'
 EMAIL_PORT = 587
 EMAIL_FROM = EMAIL_HOST_USER
